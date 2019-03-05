bf848f4ca3d32db4a861deb21a64a246509f4790
Alex Bujenita <dekc89@gmail.com>
2019-03-05 11:57:52 +0000

    backup

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,26 @@
 require_relative '../config/environment'
 
+# greeting
+# sleep 1
+# system("clear")
+# create_user
+
+def play
 greeting
 sleep 1
 system("clear")
-create_user
+user = create_user
+puts 'will start fishing'
+sleep 0.5
+user.create_game
+puts 'fish'
+sleep 0.5
+user.fishing
+user.fishing
+user.fishing
+user.fishing
+user.fishing
+user.update_total_score
+end
 
-
+play
