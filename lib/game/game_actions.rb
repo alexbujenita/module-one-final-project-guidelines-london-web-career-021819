7659c8231a17d4274d358e44d846c74b0f717af1
Alex Bujenita <dekc89@gmail.com>
2019-03-05 16:42:17 +0000

    fishing session added

--- lib/game/game_actions.rb
+++ lib/game/game_actions.rb
@@ -2,10 +2,44 @@ def play(user)
   sleep 0.5
   user.create_game
   puts 'fishing!'
-  5.times do
-    user.fishing
-  end
+  fishing_session(user)
   user.update_total_score
+end
+
+def fishing_session(user)
+  times_fished = 0
+  thrown_back_fish = 0
+  binding.pry
+  until times_fished == 5
+    user.fishing
+    fish_caught = FishType.find(GameCatch.last.fish_type_id)
+    # puts "You caught a #{fish_caught.name}!"
+    puts "-" * 80
+    case fishing_menu_when_fish_caught
+    when 1
+      puts '*' * 80
+      puts "Fish kept"
+      times_fished += 1
+    when 2
+      if thrown_back_fish == 3
+        puts "You can't throw any more fish..."
+        puts '-' * 80
+        times_fished += 1
+      elsif thrown_back_fish == 2
+        puts "This was the last fish that you could throw."
+        puts '-' * 80
+        puts "Fishy probably will survive!"
+        GameCatch.last.delete
+        thrown_back_fish +=1
+      else
+      puts "Fishy probably will survive!"
+      GameCatch.last.delete
+      thrown_back_fish +=1
+      end
+    end
+    # binding.pry
+    # 'asd'
+  end
 end
 
 def show_rules
@@ -15,7 +49,7 @@ end
 def top_ten_leaderboard
   top_ten = Game.order(total_points: :desc).limit(10)
   top_ten.each do |game|
-    puts "#{User.find(game.user_id).username}  -  #{game.total_points}"
+    puts "#{game.total_points}  - #{User.find(game.user_id).username}"
   end
 end
 
