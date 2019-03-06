75d9762e989b23c9d0f467cc4b8dc34c9a98e507
Alex Bujenita <dekc89@gmail.com>
2019-03-06 12:01:35 +0000

    game over sound

--- lib/art/voices.rb
+++ lib/art/voices.rb
@@ -19,3 +19,8 @@ def say_location(loc_id)
   voices = %w[Alex Daniel Fiona Fred Karen Moira Tessa Veena Victoria Kyoko]
   `say -v #{voices.sample} "You have chosen #{loc_name}. Good Luck!"`
 end
+
+def thanks_for_playing
+  voices = %w[Alex Daniel Fiona Fred Karen Moira Tessa Veena Victoria Kyoko]
+  `say -v #{voices.sample} "Thank you for playing, FISH FRENZY."`
+end
