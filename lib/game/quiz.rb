fbf49a7b593d99d51ec7d692aca1a4db34422d3b
Alex Bujenita <dekc89@gmail.com>
2019-03-07 12:09:30 +0000

    quotes and quiz

---
+++ lib/game/quiz.rb
@@ -0,0 +1,78 @@
+require 'pry'
+require 'tty-prompt'
+
+def quiz
+  points = first + second + third + fourth + fifth + sixth + seventh
+  binding.pry
+  'asd'
+end
+
+def first
+  prompt = TTY::Prompt.new
+  prompt.select('Would you buy this game?') do |menu|
+    menu.choice :name => 'Yes', value: 1
+    menu.choice :name => 'Never', value: -90
+  end
+end
+
+def second
+  prompt = TTY::Prompt.new
+  prompt.select('What poisonous fish is served in high end sushi restaurants?') do |menu|
+    menu.choice :name => 'Salmon', value: 0
+    menu.choice :name => 'Pufferfish', value: 1
+    menu.choice :name => 'Blue Marlin', value: 0
+    menu.choice :name => 'Shark', value: 0
+  end
+end
+
+def third
+  prompt = TTY::Prompt.new
+  prompt.select('What sign of the zodiac is represented by two fish?') do |menu|
+    menu.choice :name => 'Capricorn', value: 0
+    menu.choice :name => 'Aquarius', value: 0
+    menu.choice :name => 'Libra', value: 0
+    menu.choice :name => 'Pisces', value: 1
+  end
+end
+
+def fourth
+  prompt = TTY::Prompt.new
+  prompt.select('What is the largest fish?') do |menu|
+    menu.choice :name => 'Giant catfish', value: 0
+    menu.choice :name => 'Whale', value: 0
+    menu.choice :name => 'Whale shark', value: 1
+    menu.choice :name => 'Dolphin', value: 0
+  end
+end
+
+def fifth
+  prompt = TTY::Prompt.new
+  prompt.select('Which highly regarded food fish is the largest of all flat fish?') do |menu|
+    menu.choice :name => 'Giant catfish', value: 0
+    menu.choice :name => 'Sunfish', value: 0
+    menu.choice :name => 'Halibut', value: 1
+    menu.choice :name => 'Carp', value: 0
+  end
+end
+
+def sixth
+  prompt = TTY::Prompt.new
+  prompt.select("What does the word 'koi' mean in Japanese?") do |menu|
+    menu.choice :name => 'Carp', value: 1
+    menu.choice :name => 'Tuna', value: 0
+    menu.choice :name => 'Swimming bear', value: 0
+    menu.choice :name => 'Salmon', value: 0
+  end
+end
+
+def seventh
+  prompt = TTY::Prompt.new
+  prompt.select("What are baby salmon called when emerging from their eggs?") do |menu|
+    menu.choice :name => 'Carp', value: 1
+    menu.choice :name => 'Tuna', value: 0
+    menu.choice :name => 'Swimming bear', value: 0
+    menu.choice :name => 'Fry', value: 0
+  end
+end
+
+quiz
