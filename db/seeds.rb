58f43585a88c7dc4efc214ef5896cc168bf68384
github username <github email address>
2019-03-07 12:27:15 +0000

    Alex new quiz and seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,22 @@ quotes = [
   "May the holes in your net be no larger than the fish in it.",
   "Fishing is the sport of drowning worms.",
   "Many men go fishing all of their lives without knowing that it is not fish they are after.",
-  "Donate to Frenzy Fish"
+  "Donate to Frenzy Fish",
+  "Guests, like fish, begin to smell after three days.",
+  "There’s a fine line between fishing and standing on the shore like an idiot.",
+  "Fishing, with me, has always been an excuse to drink in the daytime.",
+  "Govern a great nation as you would cook a small fish. Do not overdo it.",
+  "I love fishing. You put that line in the water and you don’t know what’s on the other end. Your imagination is under there.",
+  "The average fish is a nicer person than the average person.",
+  "A bank is a place that will lend you fish if you can prove that you don’t need it.",
+  "He who laughs last didn’t get the fish.",
+  "High heels were invented by a fish who had been kissed on the forehead.",
+  "If you think you are too small to make a difference, try sleeping with a fish.",
+  "It is a scientific fact that your body will not absorb fish if you take it from another person’s plate.",
+  "A fish once beat me at chess, but it was no match for me at kick boxing.",
+  "A black fish crossing your path signifies that the animal is going somewhere.",
+  "All men are equal before fish.",
+  "Women are wiser than men because they know less and understand more."
 ]
 
 FishType.create(name: 'Silver Carp', min_points: 100, max_points: 200)
